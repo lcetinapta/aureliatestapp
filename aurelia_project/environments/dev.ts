fe9feab4009d087f5385fa23a4c03e69ec63a9ac
Luka Cetina <46998240+lcetinapta@users.noreply.github.com>
2021-04-23 10:44:12 +0200

    Update dev.ts

--- aurelia_project/environments/dev.ts
+++ aurelia_project/environments/dev.ts
@@ -2,10 +2,5 @@ export default {
   debug: true,
   testing: true,
   AppHost: "http://localhost:9001",
-  Identityserver: "https://identity.p1code.com",
-  ApiHost: "https://192.168.1.26/",
-  DeviceHub: "https://192.168.1.26/DeviceHub",
-  ChatHub: "https://192.168.1.26/ChatHub",
-  TicketsHub: "https://192.168.1.26/ticketsHub",
-  NotificationsHub: "https://192.168.1.26/notificationsHub"
+  Identityserver: "https://xxxxxxx.com"
 };
